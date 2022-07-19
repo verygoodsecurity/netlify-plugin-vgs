0f68aacd95d42e1657010dacc1d2623c0127a1ee
Dima Marynych <dima.marynych@verygood.systems>
2022-07-19 15:53:26 +0100

    Auto-create tokenization route for collect.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
-{
+export default {
   "vaultApiUrl": "https://api.sandbox.verygoodsecurity.com",
   "auth": {
     "tokenHost": "https://auth.verygoodsecurity.com",
     "tokenPath": "/auth/realms/vgs/protocol/openid-connect/token",
     "authorizePath": "/auth/realms/vgs/protocol/openid-connect/auth"
-  }
+  },
+  "defaultRuleTokenizeHostEndpoint": "(.*)\\.verygoodproxy\\.com",
+  "defaultRuleTokenizeHostOverrideEndpoint": "https://api.sandbox.verygoodvault.com",
+  "tokenizationAPIHostname": "api.sandbox.verygoodvault.com",
 }
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const { updateRoute } = require('./routes');
-const { getRouteConfig } = require('./utils');
+import { updateRoute, applyCollectRoute } from './routes.js';
+import { getRouteConfig } from './utils.js';
+import { NetlifyAPI } from 'netlify';
 
+export const { VGS_VAULT_ID} = process.env;
 
-const { VGS_VAULT_ID} = process.env;
+export default {
+  onPreBuild: async ({ constants }) => {
+    console.log("[vgs-plugin] onPreBuild, vaultId:", VGS_VAULT_ID);
 
-module.exports = {
-  onPreBuild: async (params) => {
-    console.log("[vgs-plugin] onPreBuild, vaultId:", VGS_VAULT_ID)
+    const { NETLIFY_API_TOKEN, SITE_ID } = constants;
+    const netlify = new NetlifyAPI(NETLIFY_API_TOKEN);
+
+    const site = await netlify.getSite({ site_id: SITE_ID });
+    console.log("[vgs-plugin] site:", site.build_settings);
     const routeConfig = getRouteConfig();
 
     if(routeConfig) {
@@ -15,5 +21,27 @@ module.exports = {
 
       console.log('[vgs-plugin] route update finished!')
     }
+
+    await applyCollectRoute(VGS_VAULT_ID);
+
+    console.log('[vgs-plugin] netlify updateSite')
+    try {
+      await netlify.updateSite(
+        {
+          site_id: SITE_ID, 
+          body: {
+            build_settings: {
+              env: {
+                ...site.build_settings.env,
+                ENV_TEST: 'test3',
+              }
+            },
+          }
+      });
+    } catch (e) {
+      console.log('[vgs-plugin] netlify updateSite error:', e)
+    }
+
+    console.log('[vgs-plugin] netlify updateSite finished!')
   },
 }
